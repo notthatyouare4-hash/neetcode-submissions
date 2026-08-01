1f30fe353ef02a44d7de41a35cd681d10119795b
notthatyouare4-hash <notthatyouare4@gmail.com>
2026-08-01 12:35:09 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res=0;
+        unordered_set<char> charset(s.begin(),s.end());
+
+        for(char c: charset){
+            int cnt=0,l=0;
+            for(int r=0; r<s.size(); r++){
+                if(s[r]==c) cnt++;
+
+                while((r-l+1) - cnt > k){
+                    if(s[l]==c) cnt--;
+                    l++;
+                }
+                res= max(res,r-l+1);
+            }
+            
+        }
+        return res;
+    }
+};
